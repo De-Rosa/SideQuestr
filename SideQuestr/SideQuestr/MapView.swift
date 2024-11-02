bb6bb473b6c5cd630252f2e2dcf1cb21578836ee
De-Rosa <135837742+De-Rosa@users.noreply.github.com>
2024-11-02 19:56:31 +0000

    Luca branch (#7)
    * live premission refresh
    
    * Add tab bar
    
    * fix mapview
    
    ---------
    
    Co-authored-by: tymekwn <tymekwn@gmail.com>

--- SideQuestr/SideQuestr/MapView.swift
+++ SideQuestr/SideQuestr/MapView.swift
@@ -9,10 +9,29 @@ import SwiftUI
 import MapKit
 import CoreLocation
 
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    private let manager = CLLocationManager()
+    @Published var isAuthorized: Bool = false
+
+    override init() {
+        super.init()
+        manager.delegate = self
+        manager.requestAlwaysAuthorization()
+        checkAuthorizationStatus()
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        checkAuthorizationStatus()
+    }
+
+    public func checkAuthorizationStatus() {
+        isAuthorized = manager.authorizationStatus == .authorizedAlways || manager.authorizationStatus == .authorizedWhenInUse
+    }
+}
+
 struct MapView: View {
+    @StateObject private var locationManager = LocationManager()
     @State private var location: MapCameraPosition = .userLocation(fallback: .automatic)
-    @State private var isAuthorised =  CLLocationManager().authorizationStatus == .authorizedAlways
-  
     @State private var message: String = ""
     @State private var userInput: String = ""
 
@@ -23,18 +42,13 @@ struct MapView: View {
                     MapUserLocationButton()
                     MapPitchToggle()
                 }
-                .onAppear() {
-                    CLLocationManager().requestAlwaysAuthorization()
-                }
             
-            VStack {
-                RoundedRectangle(cornerRadius: 50)
-                    .padding()
-            }
-            
-            if (false) {
+            if !locationManager.isAuthorized {
                 LocationNotAllowed()
             }
+        }
+        .onAppear {
+            locationManager.checkAuthorizationStatus()
         }
     }
 }
---
+++ SideQuestr/SideQuestr/TabBarView.swift
@@ -0,0 +1,25 @@
+//
+//  TabBarView.swift
+//  SideQuestr
+//
+//  Created by Luca De Rosa on 02/11/2024.
+//
+
+import SwiftUI
+
+struct TabBarView: View {
+    @Environment(\.colorScheme) var colorScheme
+    
+    var body: some View {
+        TabView {
+            MapView()
+                .tabItem { Label("Map", systemImage: "map") }
+            QuestMenuView()
+                .tabItem { Label("Quests", systemImage: "questionmark.app") }
+        }
+    }
+}
+
+#Preview {
+    TabBarView()
+}
