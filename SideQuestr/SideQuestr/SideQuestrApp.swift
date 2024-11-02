8454eefa5381deb7b40898a34923232e85c297a6
De-Rosa <lucadr222@gmail.com>
2024-11-02 13:39:00 +0000

    Initial Swift project

---
+++ SideQuestr/SideQuestr/SideQuestrApp.swift
@@ -0,0 +1,17 @@
+//
+//  SideQuestrApp.swift
+//  SideQuestr
+//
+//  Created by Luca De Rosa on 02/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct SideQuestrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
