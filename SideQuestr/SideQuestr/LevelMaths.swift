557f349c9438cb6f44c9db9327f0d6fb8a0899a0
tymekwn <tymekwn@gmail.com>
2024-11-03 07:06:25 +0000

    as branch indicates

--- SideQuestr/SideQuestr/LevelMaths.swift
+++ SideQuestr/SideQuestr/LevelMaths.swift
@@ -14,8 +14,6 @@ func plusnLog10(n: Int32) -> Int32 {
     return 10 + Int32(Double(n) * log10(Double(n)))
 }
 
-import Foundation
-
 func levelUp(lvl: Int32, exp: Int32) -> (lvl: Int32, exp: Int32) {
     // Calculate n * log10(n) as an integer
     let threshold = plusnLog10(n: lvl)
