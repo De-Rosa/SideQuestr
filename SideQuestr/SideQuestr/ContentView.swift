16f557b2521f6dde6757d5394c0d35c81772c43d
De-Rosa <lucadr222@gmail.com>
2024-11-03 01:37:35 +0000

    Add permanent storage for locations

--- SideQuestr/SideQuestr/ContentView.swift
+++ SideQuestr/SideQuestr/ContentView.swift
@@ -6,31 +6,11 @@
 //
 
 import SwiftUI
-import CoreLocation
 
-class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
-    private let manager = CLLocationManager()
-    @Published var isAuthorized: Bool = false
-    
-    override init() {
-        super.init()
-        manager.delegate = self
-        manager.requestAlwaysAuthorization()
-        checkAuthorizationStatus()
-    }
-    
-    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
-        checkAuthorizationStatus()
-    }
-
-    public func checkAuthorizationStatus() {
-        isAuthorized = manager.authorizationStatus == .authorizedAlways || manager.authorizationStatus == .authorizedWhenInUse
-    }
-}
 
 struct ContentView: View {
     @StateObject var questModel = QuestModel() // Shared instance of QuestModel
-    @StateObject private var locationManager = LocationManager()
+    @StateObject private var locationManager = LocationManager.shared
 
     var body: some View {
         ZStack {
